56a34b9e1f8b25b38e49448cf17a5f726886982c
Shack0410 <kamikuz3123@gmail.com>
2023-08-12 05:47:34 +0900

    update works-swiper2

--- js/swiper.js
+++ js/swiper.js
@@ -26,4 +26,40 @@ var swiper = new Swiper(".js-works-swiper", {
     clickable: true,
   },
 });
+
+
+/* page-works-detail */
+  //サムネイルスライド
+  var sliderThumbnail = new Swiper ('.js-gallery-thumbs', {
+    slidesPerView: 3,
+    // slidesPerView: 'auto',
+    breakpoints: {
+      // 768px以上の場合
+      768: {
+        slidesPerView: 8,
+      }
+    },
+    spaceBetween: 8,
+    centeredSlides: true,
+    loop: true,
+    // loopAdditionalSlides: 8,
+    watchSlidesProgress: true,
+    // slideToClickedSlide: true,
+    // slideThumbActiveClass:	'swiper-slide-thumb-active',
+    // thumbsContainerClass: 'gallery-thumbs'
+});
+var slider = new Swiper ('.js-gallery__slider', {
+  slidesPerView: 1,
+  centeredSlides: true,
+  loop: true,
+  // loopedSlides: 8, //スライドの枚数と同じ値を指定
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  thumbs: {
+    swiper: sliderThumbnail,
+  },
+  });
+
 });
