96d40e7457b1db237034c13a6a86826de062be2d
Shack0410 <kamikuz3123@gmail.com>
2023-08-14 07:52:55 +0900

    pupdate pagination

--- js/swiper.js
+++ js/swiper.js
@@ -1,70 +1,63 @@
 jQuery(function ($) { // この中であればWordpressでも「$」が使用可能になる
 
-var swiper = new Swiper ('.js-mv-swiper', {
-  loop: true,
-  effect: 'fade',
-  autoplay: {
-      delay: 4000,
-      disableOnInteraction: false,
-  },
-  speed: 2000,
-  fadeEffect: {
-      crossFade: true
-  }
-});
-
-var swiper = new Swiper(".js-works-swiper", {
-  loop: true,
-  effect: "fade",
-  autoplay: {
-    delay: 4000,
-    disableOnInteraction: false,
-  },
-  speed: 2000,
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-});
-
-
-/* page-works-detail */
-  //サムネイルスライド
-  var sliderThumbnail = new Swiper ('.js-gallery-thumbs', {
-    slidesPerView: 3,
-    // slidesPerView: 'auto',
-    breakpoints: {
-      768: {
-        slidesPerView: 8,
-      }
-    },
-    spaceBetween: 24,
-    breakpoints: {
-      // 768px以上の場合
-      768: {
-        spaceBetween: 8,
-      }
-    },
-    centeredSlides: true,
+  var swiper = new Swiper ('.js-mv-swiper', {
     loop: true,
-    // loopAdditionalSlides: 8,
-    watchSlidesProgress: true,
-    // slideToClickedSlide: true,
-    // slideThumbActiveClass:	'swiper-slide-thumb-active',
-    // thumbsContainerClass: 'gallery-thumbs'
-});
-var slider = new Swiper ('.js-gallery__slider', {
-  slidesPerView: 1,
-  centeredSlides: true,
-  loop: true,
-  // loopedSlides: 8, //スライドの枚数と同じ値を指定
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-  thumbs: {
-    swiper: sliderThumbnail,
-  },
+    effect: 'fade',
+    autoplay: {
+        delay: 4000,
+        disableOnInteraction: false,
+    },
+    speed: 2000,
+    fadeEffect: {
+        crossFade: true
+    }
   });
-
-});
+  
+  var swiper = new Swiper(".js-works-swiper", {
+    loop: true,
+    effect: "fade",
+    autoplay: {
+      delay: 0,
+      disableOnInteraction: false,
+    },
+    speed: 2000,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
+  
+  /* page-works-detail */
+    //サムネイルスライド
+    var sliderThumbnail = new Swiper ('.js-gallery-thumbs', {
+      loop: true,
+      slidesPerView: 2.1,
+      slidePerGroup:3,
+      spaceBetween: 24,
+      breakpoints: {
+        500: {
+          slidesPerView: 5,
+          spaceBetween: 16,
+        },
+        768: {
+          slidesPerView: 8,
+          spaceBetween: 8,
+        }
+      },
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+      centeredSlides: true,
+  });
+  var slider = new Swiper ('.js-gallery__slider', {
+    loop: true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    thumbs: {
+      swiper: sliderThumbnail,
+    },
+    });
+  });
